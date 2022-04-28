91eba6a3e083409b8378e677ded0fba7dc528baa
Alex Warnes <alex.s.warnes@gmail.com>
2022-04-28 00:04:23 -0500

    use static adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
-import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
+import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -9,6 +9,9 @@ const config = {
 
 	kit: {
 		adapter: adapter()
+	},
+	prerender: {
+		default: true
 	}
 };
 
